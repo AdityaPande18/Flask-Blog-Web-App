e74a900ff5a98e6a038cb49750c17ad1c13ce18a
Aditya Pande <adityapande88@gmail.com>
2020-07-10 19:13:31 +0530

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,8 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '30bQt8I8nZr2gx5PbIKPO3k1NGc35CxD'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
